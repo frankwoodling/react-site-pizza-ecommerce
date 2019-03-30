02291ff14024a5a82282b5d7a32e5d92ef8020a1
Frank Woodling <fwoodling@gmail.com>
2019-03-30 03:40:57 -0500

    add prettier to vscode

--- src/components/ButtonCart/ButtonCart.tsx
+++ src/components/ButtonCart/ButtonCart.tsx
@@ -1,38 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
-import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
 
 const styles = {
-    buttonCart: {
-        fontSize: 12,
-        color: 'white',
-        background: '#f04a3c',
-        boxShadow: 'none',
-        borderRadius: 50,
-        margin: '30px',
-        textTransform: 'capitalize',
-    },
-    shoppingCart: {
-        paddingRight: '10px'
-    }
-}
+  buttonCart: {
+    fontSize: 12,
+    color: "white",
+    background: "#f04a3c",
+    boxShadow: "none",
+    borderRadius: 50,
+    margin: "30px",
+    textTransform: "capitalize"
+  },
+  shoppingCart: {
+    paddingRight: "10px"
+  }
+};
 
 const ButtonCart: React.FC = () => {
-    return (
-            <Button
-                style={styles.buttonCart}
-                size="large"
-                variant="raised">
-            <ShoppingCart style={styles.shoppingCart} />
-            Add to Cart
-            </Button>     
-    )
-}
+  return (
+    <Button style={styles.buttonCart} size="large" variant="raised">
+      <ShoppingCart style={styles.shoppingCart} />
+      Add to Cart
+    </Button>
+  );
+};
 
 ButtonCart.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+  classes: PropTypes.object.isRequired
+};
 
 export { ButtonCart };
