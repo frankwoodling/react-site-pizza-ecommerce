31a9aa1d28669cb5489c18a879719d91c644b473
Frank Woodling <fwoodling@gmail.com>
2019-03-30 03:03:58 -0500

    add cart icon to button with inline style

--- src/components/ButtonCart/ButtonCart.tsx
+++ src/components/ButtonCart/ButtonCart.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
 
-const style = {
+const styleButtonCart = {
+    fontSize: 12,
     color: 'white',
     background: '#f04a3c',
     boxShadow: 'none',
-    borderRadius: 50
+    borderRadius: 50,
+    //width: 50
+    margin: '30px'
 }
+
+const styleShoppingCart = {
+    paddingRight: '10px'
+}
+
 
 const ButtonCart: React.FC = () => {
     return (
             <Button
-                style={style}
+                style={styleButtonCart}
                 size="large"
                 variant="raised">
+            <ShoppingCart style={styleShoppingCart} />
             Add to Cart
             </Button>
+            
     )
 
 }
+
+ButtonCart.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
 
 export { ButtonCart };
