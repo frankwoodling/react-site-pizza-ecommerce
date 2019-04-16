5a66c75dc480c0b42b4bced2112390cb6efa587d
Frank Woodling <fwoodling@gmail.com>
2019-04-15 23:41:07 -0500

    update cart button name

--- src/components/ButtonCart/ButtonCart.stories.tsx
+++
@@ -1,7 +0,0 @@
-// TODO: ButtonCart knobs.
-// also need an interface that supports multiple sizes
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { ButtonCart } from "./index";
-
-storiesOf("Button", module).add("Cart", () => <ButtonCart />);
