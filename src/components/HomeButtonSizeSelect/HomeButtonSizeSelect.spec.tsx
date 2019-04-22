481012dac2e80dafc27836210dde4a0de683dc4f
Frank Woodling <fwoodling@gmail.com>
2019-04-21 23:47:58 -0500

    add test boilerplate

--- src/components/HomeButtonSizeSelect/HomeButtonSizeSelect.spec.tsx
+++ src/components/HomeButtonSizeSelect/HomeButtonSizeSelect.spec.tsx
@@ -1,12 +1,29 @@
-/*
-TODO: Write ButtonCart test
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ButtonCart from './';
+import React from "react";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { HomeButtonSizeSelect } from "./index";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ButtonCart />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("HomeButtonSizeSelect", () => {
+  beforeAll(() => {
+    configure({ adapter: new Adapter() });
+  });
+  it("should render a button with 50px height and width", () => {
+    // arrange
+    const props = {
+      buttonSize: "50px",
+      backgroundColor: "#FFCD00",
+      lineHeight: "14px",
+      textTop: 22,
+      textBottom: "cm"
+    };
+
+    const component = mount(<HomeButtonSizeSelect {...props} />);
+
+    // act
+    component.update();
+
+    //assert
+    console.log(component.debug());
+    expect(component.prop("height")).toEqual("50px");
+  });
 });
-*/
